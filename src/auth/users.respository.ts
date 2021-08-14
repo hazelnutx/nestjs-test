067a0ddbe05333255456e715216966f4f2f045a5
Raul Rusu <raulrusu46@gmail.com>
2021-08-14 21:42:43 +0300

    signup and signin

---
+++ src/auth/users.respository.ts
@@ -0,0 +1,35 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+
+@EntityRepository(User)
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({
+      username,
+      password: hashedPassword,
+    });
+
+    // Try and save the user if the user is unique, otherwise throw error
+    try {
+      await this.save(user);
+    } catch (e) {
+      // duplicate username
+      if (e.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
